b4e970918c5fe519d208face31f93fa1e1aa36f9
Dominic Karma <farfetchedking541@gmail.com>
2025-03-27 15:46:41 -0500

    Add wip music

---
+++ Content/NPCs/ExoMechs/SpecificManagers/CustomExoMechsMusicScene.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WoTM.Content.NPCs.ExoMechs.FightManagers;
+
+namespace WoTM.Content.NPCs.ExoMechs.SpecificManagers
+{
+    public class CustomExoMechsMusicScene : ModSceneEffect
+    {
+        public override SceneEffectPriority Priority => SceneEffectPriority.BossHigh;
+
+        public override float GetWeight(Player player) => 0.93f;
+
+        public override int Music => MusicLoader.GetMusicSlot("WoTM/Assets/Sounds/Music/HEAVYDUTY");
+
+        public sealed override void SetupContent() => MusicID.Sets.SkipsVolumeRemap[Music] = true;
+
+        public override bool IsSceneEffectActive(Player player)
+        {
+            MusicID.Sets.SkipsVolumeRemap[Music] = true;
+            return ExoMechFightStateManager.FightOngoing && CustomExoMechsSky.Opacity >= 0.05f;
+        }
+    }
+}
