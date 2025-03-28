4dd57ef54355a28879e799bf0f093a128086947e
Dominic Karma <farfetchedking541@gmail.com>
2025-03-27 19:23:34 -0500

    Remove boss checklist message

---
+++ Core/CrossCompatibility/BossChecklistCompatibility.cs
@@ -0,0 +1,51 @@
+﻿using System.Reflection;
+using Luminance.Core.Hooking;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using WoTM.Content.NPCs.ExoMechs.FightManagers;
+
+namespace WoTM.Core.CrossCompatibility;
+
+public class BossChecklistCompatibility : ModSystem
+{
+    /// <summary>
+    /// The boss checklist mod.
+    /// </summary>
+    public static Mod BossChecklistMod
+    {
+        get;
+        private set;
+    }
+
+    internal delegate LocalizedText? GetDespawnMessageOrig(object instance, NPC npc);
+
+    internal delegate LocalizedText? GetDespawnMessageHook(GetDespawnMessageOrig orig, object instance, NPC npc);
+
+    public override void PostSetupContent()
+    {
+        if (!ModLoader.TryGetMod("BossChecklist", out Mod bc))
+            return;
+        BossChecklistMod = bc;
+
+        ChangeDraedonDespawnMessage();
+    }
+
+    [JITWhenModsEnabled("BossChecklist")]
+    private void ChangeDraedonDespawnMessage()
+    {
+        MethodInfo? despawnMessageMethod = BossChecklistMod.Code.GetType("BossChecklist.EntryInfo")?.GetMethod("GetDespawnMessage", LumUtils.UniversalBindingFlags);
+        if (despawnMessageMethod is null)
+            Mod.Logger.Warn("Could not find Boss Checklist's boss despawn message method.");
+        else
+        {
+            HookHelper.ModifyMethodWithDetour(despawnMessageMethod, new GetDespawnMessageHook((orig, instance, npc) =>
+            {
+                if (ExoMechNPCIDs.ExoMechIDs.Contains(npc.type))
+                    return null;
+
+                return orig(instance, npc);
+            }));
+        }
+    }
+}
